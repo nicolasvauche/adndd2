acb699eb01bc6b1a212744d891f003ffef4a4a3e
Nicolas vauché <hello@nicolasvauche.net>
2021-07-27 20:19:38 +0200

    Scenario FormType start

---
+++ src/Form/ScenarioType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Scenario;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ScenarioType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('campaign')
+            ->add('name', TextType::class,
+                [
+                    'required' => true,
+                    'label' => 'play.game.name.label',
+                    'attr' => [
+                        'class' => 'app_form_control',
+                        'placeholder' => 'play.game.name.placeholder',
+                    ],
+                ])
+            ->add('status')
+            ->add('isPrivate')
+            ->add('startAt');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Scenario::class,
+        ]);
+    }
+}
