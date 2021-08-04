197828552ca49ae28e60b1d6f75b22cb9b27d235
matt co <87700a@gmail>
2021-08-04 20:56:56 +0200

    Fix fixture game2

--- src/DataFixtures/CharacterFixtures.php
+++ src/DataFixtures/CharacterFixtures.php
@@ -34,18 +34,18 @@ class CharacterFixtures extends Fixture implements OrderedFixtureInterface
             '27', '', 'Issue  d’une  famille  modeste  qui  a  quitté 
             l’Alsace pour rester française, Maryse rêvait d’aviation  et  de  records  en  admirant  les  appareils  militaires  en  vol.  Sans  diplôme  
             et sans argent, elle a financé son brevet de 
-            pilote en devenant mécanicienne.', true, '', 'France', 'Alsace', '200', 'game1'],
+            pilote en devenant mécanicienne.', true, '', 'France', 'Alsace', '200', 'game2'],
             ['Lord Duncan Blight', 'duncan.png', 'M', '', '1m81', '82kg', 'Duncan est un homme de haute stature, à la mise soignée et aux manières distinguées, 
             mais  son  visage  porte  les  stigmates  de  la  guerre.  L’élégance  de  ses  costumes  peine  
             à dissimuler une nature plus brutale et on l’imagine tout autant à son aise en bras de chemise dans une guerre des gangs que dans un salon de thé.', 
             '42', 'Berline ordinaire', 'Ancien  colonel  de  l’armée  britannique, Duncan est issu d’une riche famille aristo-
-            cratique. Grand amateur de rugby et de boxe, malgré des états de services irréprochables, il a quitté l’armée pour acheter une salle de boxe à New-York.', true, '', 'New-York', '', '200', 'game1'],
+            cratique. Grand amateur de rugby et de boxe, malgré des états de services irréprochables, il a quitté l’armée pour acheter une salle de boxe à New-York.', true, '', 'New-York', '', '200', 'game2'],
             ['Francesca Petrini', 'francesca.png', 'F', '', '1m67', '57kg', 'Francesca  est  une  jeune  femme  élégante  
             mais timide, qui cache son embarras sous un chapeau à large bord. Toutefois, celui qui parvient à croiser son regard en mesure im-médiatement toute l’acuité, c’est alors géné-
             ralement à son tour de baisser les yeux.', '31', '', 'À la mort de son père, cette jeune italienne 
             a pris la décision de terminer l’ouvrage que 
             son paternel avait commencé à écrire sur les rites funéraires des indiens d’Amérique. Dès lors, elle s’est installée à New-York afin de mener ce projet à bien et de profiter de la 
-            large communauté italienne qui y réside.', true, '', 'New-York', 'Italie', '200', 'game1'],
+            large communauté italienne qui y réside.', true, '', 'New-York', 'Italie', '200', 'game2'],
             ['John Prentiss', 'john.png', 'M', '', '1m74', '70kg', 'La  mâchoire  carrée,  le  regard  franc  et  la parole facile, John est un jeune homme athlé-
             tique qui semble partout à son aise. Chemise entrouverte, chapeau de cuir, il soigne son allure de baroudeur et ne se sépare jamais 
             d’un carnet dans lequel il prend note de ses 
@@ -53,7 +53,7 @@ class CharacterFixtures extends Fixture implements OrderedFixtureInterface
             a  été  élevé  par  une  nourrice  noire.  Il  en  a  
             conçu une grande amertume pour les théo-
             ries racistes de sa famille et, très jeune, est parti explorer le monde, relatant ses voyages dans des romans qui connaissent un succès 
-            d’estime.', true, '', '', '', '200', 'game1'],  
+            d’estime.', true, '', '', '', '200', 'game2'],  
             ['Ilia Droudji', 'ilia.png', 'F', '', '1m63', '49kg', 'Avec  sa  longue  chevelure  noire,  sa  dé-
             marche souple et son regard d’orientale, Ilia possède un charme explosif. Elle arbore tou-
             tefois des tenues plutôt discrètes, dans des tons de verts et de bruns. Elle possède l’éton-
@@ -61,12 +61,12 @@ class CharacterFixtures extends Fixture implements OrderedFixtureInterface
             '23', '', 'Née à Constantinople, d’une famille israé-
             lite de l’Empire Ottoman, Ilia et sa famille ont émigré à New-York peu de temps avant 
             la  dissolution  de  l’empire.  Douée  pour  les  langues,  la  jeune  femme  déracinée  y  est  devenue journaliste.', true, '', 
-            'New-York', 'Constantinople', '200', 'game1'], 
+            'New-York', 'Constantinople', '200', 'game2'], 
             ['Professeur Johan Kerenski', 'kerenski.png', 'M', '', '1m86', '71kg', 'Grand et maigre, le sourcil broussailleux, le professeur Kerenski semble en permanence absorbé par des détails auxquels lui seul est sensible. Son costume gris, austère et chif-
             fonné, suggère qu’il n’a pas dormi depuis longtemps ou alors tout habillé, tandis que son regard est habité d’une flamme d’une 
             rare intensité.', '47', '', 'Passionné  de  paranormal  et  de  métaphy-
             sique, le jeune Johan s’est intéressé très tôt aux  théories  modernes  de  psychologie.  Il tente désormais de créer sa propre méthode thérapeutique en travaillant plus particuliè-rement  sur  les  maladies  mentales  dans  les  civilisations préindustrielles.', true, '', 
-            '', '', '200', 'game1'],              
+            '', '', '200', 'game2'],              
         ];
 
         foreach ($tabCharac as list($a, $b, $c, $d, $e, $f, $g, $h, $i, $j, $k, $l, $m, $n, $o, $p)) {
