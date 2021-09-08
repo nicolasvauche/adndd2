72386e29d6624cff7cbc04b9a59ada755e8c08ef
Nicolas vauché <hello@nicolasvauche.net>
2021-09-08 21:17:00 +0200

    Campaigns

--- src/Controller/CampaignController.php
+++ src/Controller/CampaignController.php
@@ -2,25 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Campaign;
 use App\Entity\Game;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/campaign")
+ * @Route("/jouer-en-ligne/mes-campagnes")
  */
 class CampaignController extends AbstractController
 {
     /**
-     * @Route("/{id}", name="campaign")
+     * @Route("/", name="campaign")
      */
-    public function index(Game $game): Response
+    public function index(): Response
     {
+        $campaigns = $this->getDoctrine()->getRepository(Campaign::class)->findAll();
         $games = $this->getDoctrine()->getRepository(Game::class)->findAll();
 
         return $this->render('play/campaign/index.html.twig', [
-            'game' => $game,
+            'campaigns' => $campaigns,
             'games' => $games,
         ]);
     }
