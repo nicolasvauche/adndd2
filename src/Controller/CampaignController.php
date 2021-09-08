fe67cf3179bfd3e2fb4355490b672871feb409b1
Nicolas vauché <hello@nicolasvauche.net>
2021-09-08 21:49:22 +0200

    Campaign Create / edit

--- src/Controller/CampaignController.php
+++ src/Controller/CampaignController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Campaign;
 use App\Entity\Game;
+use App\Form\CampaignType;
+use App\Repository\CampaignRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @Route("/jouer-en-ligne/mes-campagnes")
@@ -26,6 +30,69 @@ class CampaignController extends AbstractController
             'campaigns' => $campaigns,
             'games' => $games,
         ]);
+    }
+
+    /**
+     * @Route("/cree-une-campagne/{gameId}", name="user.campaign.create")
+     */
+    public function create(CampaignRepository $campaignRepository, Request $request, ValidatorInterface $validator, EntityManagerInterface $manager, $gameId = null): Response
+    {
+        $campaign = new Campaign();
+        $form = $this->createForm(CampaignType::class, $campaign);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager->persist($campaign);
+            $manager->flush();
+
+            $this->addFlash('success', 'La campagne a été créée !');
+
+            return $this->redirectToRoute('user.campaign');
+        } else {
+            $errors = $validator->validate($form);
+
+            if (count($errors) > 0) {
+                $this->addFlash('danger', $errors[0]->getMessage());
+            }
+        }
+
+        return $this->render('play/campaign/form.html.twig',
+            [
+                'mode' => 'add',
+                'form' => $form->createView(),
+                'campaign' => $campaign,
+            ]);
+    }
+
+    /**
+     * @Route("/modifie-une-campagne/{id}", name="user.campaign.edit")
+     */
+    public function edit(Request $request, ValidatorInterface $validator, EntityManagerInterface $manager, Campaign $campaign)
+    {
+        $form = $this->createForm(CampaignType::class, $campaign);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($campaign);
+            $manager->flush();
+
+            $this->addFlash('success', 'La campagne a été modifiée.');
+
+            return $this->redirectToRoute('user.campaigns');
+        } else {
+            $errors = $validator->validate($form);
+
+            if (count($errors) > 0) {
+                $this->addFlash('danger', $errors[0]->getMessage());
+            }
+        }
+        return $this->render('play/campaign/form.html.twig',
+            [
+                'mode' => 'edit',
+                'form' => $form->createView(),
+                'campaign' => $campaign,
+            ]);
     }
 
     /**
