e8aa517a8c2602e1cd134e15121eae13dad5f924
matt co <87700a@gmail>
2021-09-15 10:47:42 +0200

    Fix CharacterFixtures

--- src/DataFixtures/CharacterFixtures.php
+++ src/DataFixtures/CharacterFixtures.php
@@ -53,41 +53,32 @@ class CharacterFixtures extends Fixture implements OrderedFixtureInterface
             et sans argent, elle a financé son brevet de 
             pilote en devenant mécanicienne.', true, '', 'France', 'Alsace', '200', 'game2',''],
 
-            ['Lord Duncan Blight', 'duncan.jpg', 'homme', '', '1m81', '82kg', 'Duncan est un h de haute stature, à la mise soignée et aux manières distinguées, 
+            ['Lord Duncan Blight', 'duncan.jpg', 'homme', '', '1m81', '82kg', 'Duncan est un homme de haute stature, à la mise soignée et aux manières distinguées, 
             mais  son  visage  porte  les  stigmates  de  la  guerre.  L’élégance  de  ses  costumes  peine  
             à dissimuler une nature plus brutale et on l’imagine tout autant à son aise en bras de chemise dans une guerre des gangs que dans un salon de thé.',
                 '42', '', 'Militaire - Ancien  colonel  de  l’armée  britannique, Duncan est issu d’une riche famille aristo-
             cratique. Grand amateur de rugby et de boxe, malgré des états de services irréprochables, il a quitté l’armée pour acheter une salle de boxe à New-York.', true, '', 'New-York', '', '200', 'game2',''],
 
-            ['Francesca Petrini', 'francesca.jpg', 'femme', '', '1m67', '57kg', 'Francesca  est  une  jeune  f  élégante  
-            mais timide, qui cache son embarras sous un chapeau à large bord. Toutefois, celui qui parvient à croiser son regard en mesure im-médiatement toute l’acuité, c’est alors géné-
-            ralement à son tour de baisser les yeux.', '31', '', 'Archéologue - À la mort de son père, cette jeune italienne 
+            ['Francesca Petrini', 'francesca.jpg', 'femme', '', '1m67', '57kg', 'Francesca  est  une  jeune  femme  élégante  
+            mais timide, qui cache son embarras sous un chapeau à large bord. Toutefois, celui qui parvient à croiser son regard en mesure immédiatement toute l’acuité, c’est alors généralement à son tour de baisser les yeux.', '31', '', 'Archéologue - À la mort de son père, cette jeune italienne 
             a pris la décision de terminer l’ouvrage que 
             son paternel avait commencé à écrire sur les rites funéraires des indiens d’Amérique. Dès lors, elle s’est installée à New-York afin de mener ce projet à bien et de profiter de la 
             large communauté italienne qui y réside.', true, '', 'New-York', 'Italie', '200', 'game2',''],
 
-            ['John Prentiss', 'john.jpg', 'homme', '', '1m74', '70kg', 'La  mâchoire  carrée,  le  regard  franc  et  la parole facile, John est un jeune h athlé-
-            tique qui semble partout à son aise. Chemise entrouverte, chapeau de cuir, il soigne son allure de baroudeur et ne se sépare jamais 
+            ['John Prentiss', 'john.jpg', 'homme', '', '1m74', '70kg', 'La  mâchoire  carrée,  le  regard  franc  et  la parole facile, John est un jeune homme athlétique qui semble partout à son aise. Chemise entrouverte, chapeau de cuir, il soigne son allure de baroudeur et ne se sépare jamais 
             d’un carnet dans lequel il prend note de ses 
             exploits, réels ou imaginaires.', '29', '', 'Aventurier - Fils  d’un  riche  industriel  américain,  John 
             a  été  élevé  par  une  nourrice  noire.  Il  en  a  
-            conçu une grande amertume pour les théo-
-            ries racistes de sa famille et, très jeune, est parti explorer le monde, relatant ses voyages dans des romans qui connaissent un succès 
+            conçu une grande amertume pour les théories racistes de sa famille et, très jeune, est parti explorer le monde, relatant ses voyages dans des romans qui connaissent un succès 
             d’estime.', true, '', '', '', '200', 'game2',''],
 
-            ['Ilia Droudji', 'ilia.jpg', 'femme', '', '1m63', '49kg', 'Avec  sa  longue  chevelure  noire,  sa  dé-
-            marche souple et son regard d’orientale, Ilia possède un charme explosif. Elle arbore tou-
-            tefois des tenues plutôt discrètes, dans des tons de verts et de bruns. Elle possède l’éton-
-            nante capacité de savoir à la fois disparaître dans les ombres en un clin d’œil et de briller de mille feux en société.',
-                '23', '', 'Journaliste - Née à Constantinople, d’une famille israé-
-            lite de l’Empire Ottoman, Ilia et sa famille ont émigré à New-York peu de temps avant 
-            la  dissolution  de  l’empire.  Douée  pour  les  langues,  la  jeune  f  déracinée  y  est  devenue journaliste.', true, '',
+            ['Ilia Droudji', 'ilia.jpg', 'femme', '', '1m63', '49kg', 'Avec  sa  longue  chevelure  noire,  sa  démarche souple et son regard d’orientale, Ilia possède un charme explosif. Elle arbore toutefois des tenues plutôt discrètes, dans des tons de verts et de bruns. Elle possède l’étonnante capacité de savoir à la fois disparaître dans les ombres en un clin d’œil et de briller de mille feux en société.',
+                '23', '', 'Journaliste - Née à Constantinople, d’une famille israélite de l’Empire Ottoman, Ilia et sa famille ont émigré à New-York peu de temps avant 
+            la  dissolution  de  l’empire.  Douée  pour  les  langues,  la  jeune  femme  déracinée  y  est  devenue journaliste.', true, '',
                 'New-York', 'Constantinople', '200', 'game2',''],
 
-            ['Professeur Johan Kerenski', 'kerenski.jpg', 'homme', '', '1m86', '71kg', 'Grand et maigre, le sourcil broussailleux, le professeur Kerenski semble en permanence absorbé par des détails auxquels lui seul est sensible. Son costume gris, austère et chif-
-            fonné, suggère qu’il n’a pas dormi depuis longtemps ou alors tout habillé, tandis que son regard est habité d’une flamme d’une 
-            rare intensité.', '47', '', 'Thérapeute - Passionné  de  paranormal  et  de  métaphy-
-            sique, le jeune Johan s’est intéressé très tôt aux  théories  modernes  de  psychologie.  Il tente désormais de créer sa propre méthode thérapeutique en travaillant plus particuliè-rement  sur  les  maladies  mentales  dans  les  civilisations préindustrielles.', true, '',
+            ['Professeur Johan Kerenski', 'kerenski.jpg', 'homme', '', '1m86', '71kg', 'Grand et maigre, le sourcil broussailleux, le professeur Kerenski semble en permanence absorbé par des détails auxquels lui seul est sensible. Son costume gris, austère et chiffonné, suggère qu’il n’a pas dormi depuis longtemps ou alors tout habillé, tandis que son regard est habité d’une flamme d’une 
+            rare intensité.', '47', '', 'Thérapeute - Passionné  de  paranormal  et  de  métaphysique, le jeune Johan s’est intéressé très tôt aux  théories  modernes  de  psychologie.  Il tente désormais de créer sa propre méthode thérapeutique en travaillant plus particulièrement  sur  les  maladies  mentales  dans  les  civilisations préindustrielles.', true, '',
                 '', '', '200', 'game2', ''],
         ];
         $z = 0;
